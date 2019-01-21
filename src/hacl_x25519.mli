458677efa4bfce00621dfa0c434718ab1a5d8d11
Etienne Millon <me@emillon.org>
2019-01-21 19:24:38 +0100

    WIp

--- src/hacl_x25519.mli
+++ src/hacl_x25519.mli
@@ -1,1 +1,6 @@
 val scalarmult_alloc : priv:Cstruct.t -> pub:Cstruct.t -> Cstruct.t
+(** Perform Diffie-Hellman key exchange between a private part and a public
+    part.
+
+    The private part 
+*)
