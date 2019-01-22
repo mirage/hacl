8a6291ed0b35209093da4f13f47c4a7dd672fd61
Etienne Millon <me@emillon.org>
2019-01-22 14:03:40 +0100

    Remove alcotest

--- test_wycheproof/wycheproof_hacl.ml
+++ test_wycheproof/wycheproof_hacl.ml
@@ -1,23 +1,20 @@
 open Wycheproof
 
-let hex = Alcotest.testable Wycheproof.pp_hex Wycheproof.equal_hex
+let check pp equal name expected got =
+  if equal expected got then Format.printf "%s - ok\n" name
+  else
+    Format.printf "%s - error\nexpected:\n%a\ngot:\n%a\n" name pp expected pp
+      got
 
-let test ~private_ ~public ~expected () =
+let run_test {tcId; comment; private_; public; shared = expected; _} =
+  let name = Printf.sprintf "%d - %s" tcId comment in
   let result =
     Hacl_x25519.scalarmult_alloc
       ~priv:(Cstruct.of_string private_)
       ~pub:(Cstruct.of_string public)
   in
   let got = Cstruct.to_string result in
-  Alcotest.check hex "should be equal" expected got
+  check Wycheproof.pp_hex Wycheproof.equal_hex name expected got
 
-let make_test {tcId; comment; private_; public; shared; _} =
-  let name = Printf.sprintf "%d - %s" tcId comment in
-  (name, `Quick, test ~private_ ~public ~expected:shared)
-
-let tests =
-  x25519.testGroups
-  |> List.map (fun group -> List.map make_test group.tests)
-  |> List.concat
-
-let () = Alcotest.run "Wycheproof-hacl-x25519" [("test vectors", tests)]
+let () =
+  List.iter (fun group -> List.iter run_test group.tests) x25519.testGroups
