9455f400a104401f4d1eabd31b8ed17facc23bbe
Etienne Millon <me@emillon.org>
2020-02-12 17:19:22 +0100

    Bench: use stdlib-shims (#26)

--- bench/bench.ml
+++ bench/bench.ml
@@ -1,6 +1,6 @@
 let read_urandom n =
-  let ic = Pervasives.open_in_bin "/dev/urandom" in
-  let s = Pervasives.really_input_string ic n in
+  let ic = Stdlib.open_in_bin "/dev/urandom" in
+  let s = Stdlib.really_input_string ic n in
   close_in ic;
   Cstruct.of_string s
 
