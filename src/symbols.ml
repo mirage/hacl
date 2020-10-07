aa4ffdca46179979fdf33dfb0e71cc293e7b2029
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-10-07 15:47:49 +0200

    Handle 32-bits platform nm's output

--- src/symbols.ml
+++ src/symbols.ml
@@ -10,6 +10,28 @@ let null = Unix.openfile "/dev/null" Unix.[ O_RDONLY ] 0o644
 
 let kstrf k fmt = Format.kasprintf k fmt
 
+let add_sub s ~start ~stop acc =
+  if start = stop then acc else String.sub s start (stop - start) :: acc
+
+let cuts ~sep s =
+  let sep_len = String.length sep in
+  let s_len = String.length s in
+  let max_sep_idx = sep_len - 1 in
+  let max_s_idx = s_len - sep_len in
+  let rec check_sep start i k acc =
+    if k > max_sep_idx then
+      let new_start = i + sep_len in
+      scan new_start new_start (add_sub s ~start ~stop:i acc)
+    else if s.[i + k] = sep.[k] then check_sep start i (succ k) acc
+    else scan start (succ i) acc
+  and scan start i acc =
+    if i > max_s_idx then
+      if start = 0 then [] else List.rev (add_sub s ~start ~stop:s_len acc)
+    else if s.[i] = sep.[0] then check_sep start i 1 acc
+    else scan start (i + 1) acc
+  in
+  scan 0 0 []
+
 let symbols obj =
   let fd, stdout = Unix.pipe () in
   let stderr = stdout in
@@ -30,7 +52,11 @@ let symbols obj =
   in
   let res = go [] in
   Unix.close fd;
-  List.map (fun line -> String.sub line 19 (String.length line - 19)) res
+  List.fold_left
+    (fun acc line ->
+      match List.rev (cuts ~sep:" " line) with v :: _ -> v :: acc | [] -> acc)
+    [] res
+  |> List.rev
 
 let generate ~prefix symbols output =
   let oc = open_out output in
